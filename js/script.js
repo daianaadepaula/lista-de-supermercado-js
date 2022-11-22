64d27f21bc2ffd052ed3c1792c8d8772cee4d54e
Daiana de Paula <daianaadepaula1@gmail.com>
2022-11-22 08:02:06 -0300

    js list products

--- js/script.js
+++ js/script.js
@@ -1,12 +1,41 @@
 let items = []
 
 document.querySelector('input[type=submit]').addEventListener('click', ()=>{
-	let nameProduct = document.querySelector('input[name=name_product]').value;
-	let priceProduct = document.querySelector('input[name=value_product]').value;
+	let nameProduct = document.querySelector('input[name=name_product]');
+	let priceProduct = document.querySelector('input[name=price]');
 
 	items.push({
-		nome: nameProduct,
-		valor: priceProduct
+		nome: nameProduct.value,
+		valor: priceProduct.value
 	})
-	alert(items[0].nome)
+
+	/*
+	<div class="list-product-single">
+		<h3>RedBull</h3>
+		<h3 class="price-product"><span>R$ 20,00</span></h3>
+	</div>
+	*/
+
+	let listProducts = document.querySelector('.list-products');
+	let sum = 0;
+
+	listProducts.innerHTML = ""
+	items.map(function(val){
+		sum+=parseFloat(val.valor);
+
+		listProducts.innerHTML+=`
+		<div class="list-product-single">
+			<h3>`+val.nome+`</h3>
+			<h3 class="price-product"><span>R$ `+val.valor+`</span></h3>
+		</div>
+		`;
+	})
+
+	sum = sum.toFixed(2)
+	nameProduct.value = ""
+	priceProduct.value = ""
+
+	let elementSum = document.querySelector('.sum-product h1');
+	elementSum.innerHTML = `R$ `+sum;
+
 })
